82dd0bacb5845334aecf13ee05dfb73434f2b249
Maciej Podsedkowski <mpodsedkowski@gmail.com>
2019-11-21 13:27:38 +0000

    Matrix test improve

--- Matrix/TestMatrix.cpp
+++ Matrix/TestMatrix.cpp
@@ -1,9 +1,19 @@
 #include <Matrix.h>
+#include <cassert>
 
 using namespace std;
 
+void testReadFromFile(Matrix& m2x3, Matrix& m3x3);
 void test(const Matrix& m2x2, const Matrix& m2x3, const Matrix& m3x3);
-void display(const Matrix& m1);
+void display(const Matrix& m2x2, const Matrix& m2x3, const Matrix& m3x3);
+void testComparison(const Matrix& m2x2, const Matrix& m2x3, const Matrix& m3x3);
+void testAdd(const Matrix& m2x2, const Matrix& m2x3, const Matrix& m3x3);
+void testSubstract(const Matrix& m2x2, const Matrix& m2x3, const Matrix& m3x3);
+void testMultiply(const Matrix& m2x2, const Matrix& m2x3, const Matrix& m3x3);
+void testAddEqual(const Matrix& m2x2, const Matrix& m2x3, const Matrix& m3x3);
+void testSubstractEqual(const Matrix& m2x2, const Matrix& m2x3, const Matrix& m3x3);
+void testMultiplyEqual(const Matrix& m2x2, const Matrix& m2x3, const Matrix& m3x3);
+
 
 int main(){
     Matrix m2x2;
@@ -14,9 +24,72 @@ int main(){
 
     Matrix m2x3;
     Matrix m3x3;
+    testReadFromFile(m2x3, m3x3);
     test(m2x2, m2x3, m3x3);
 }
 
-void display(const Matrix m1){
-    cout << "Matrix 2x2:" << endl << m1 << endl;
+void testReadFromFile(Matrix& m2x3, Matrix& m3x3){
+
+}
+
+void test(const Matrix& m2x2, const Matrix& m2x3, const Matrix& m3x3){
+    display(m2x2, m2x3, m3x3);
+    testComparison(m2x2, m2x3, m3x3);
+    testAdd(m2x2, m2x3, m3x3);
+    testSubstract(m2x2, m2x3, m3x3);
+    testAddEqual(m2x2, m2x3, m3x3);
+    testSubstractEqual(m2x2, m2x3, m3x3);
+    testMultiply(m2x2, m2x3, m3x3);
+    testMultiplyEqual(m2x2, m2x3, m3x3);
+}
+
+void display(const Matrix& m2x2, const Matrix& m2x3, const Matrix& m3x3){
+    cout << "Matrix 2x2:" << endl << m2x2 << endl;
+    cout << "Matrix 2x3:" << endl << m2x3 << endl;
+    cout << "Matrix 3x3:" << endl << m3x3 << endl;
+}
+
+void testComparison(const Matrix& m2x2, const Matrix& m2x3, const Matrix& m3x3){
+    Matrix t1 = m2x2;
+    assert(t1 == t1);
+    assert(t1 == m2x2);
+    assert(!(t1 == m3x3));
+    t1(1, 1) = 50;
+    assert(!(t1 == m2x2));
+
+    Matrix t2 = m3x3;
+    assert(!(m3x3 != m3x3));
+    assert(m2x2 != m2x3);
+    assert(!(t2 == m3x3));
+    t2(2, 2) = 50;
+    assert(m3x3 != t2);
+
+    Matrix t3 = 5;
+    assert(t3 == 5);
+    assert(5 == t3);
+    assert(t3 != 6);
+    assert(6 != t3);
+}
+
+void testAdd(const Matrix& m2x2, const Matrix& m2x3, const Matrix& m3x3){
+
+}
+
+void testSubstract(const Matrix& m2x2, const Matrix& m2x3, const Matrix& m3x3){
+
+}
+
+void testMultiply(const Matrix& m2x2, const Matrix& m2x3, const Matrix& m3x3){
+
+}
+
+void testAddEqual(const Matrix& m2x2, const Matrix& m2x3, const Matrix& m3x3){
+
+}
+
+void testSubstractEqual(const Matrix& m2x2, const Matrix& m2x3, const Matrix& m3x3){
+
+}
+void testMultiplyEqual(const Matrix& m2x2, const Matrix& m2x3, const Matrix& m3x3){
+
 }

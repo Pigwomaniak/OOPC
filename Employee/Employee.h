3960c0272c9ebf6a76db0362d827236243548fdc
Maciej Podsedkowski <mpodsedkowski@gmail.com>
2019-12-15 14:41:51 +0000

    map first compile

---
+++ Employee/Employee.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <utility>
+using namespace std;
+
+class Employee{
+private:
+
+public:
+    string name;
+    string position;
+    int age;
+
+    Employee(string  newName, string  newPosition, const int newAge)
+    :name(std::move(newName)), position(std::move(newPosition)), age(newAge){};
+
+    ~Employee(){
+
+    }
+
+    friend ostream&operator << (ostream& out, const Employee& src){
+        out << src.name << "\t" << src.position << "\t" << src.age << endl;
+        return out;
+    }
+
+    bool operator == (const Employee& second) const{
+        return (name == second.name && position == second.position && age == second.age);
+    }
+    bool operator != (const Employee& second) const{
+        return (name != second.name || position != second.position || age != second.age);
+    }
+};
--- Employee/Map.h
+++ Employee/Map.h
@@ -1,13 +1,10 @@
+#pragma once
+#include <iostream>
+using namespace std;
+
 template <class kType,class eType>
 
 class Map {
-    /*
-    typedef struct cell {
-        cell* next;
-        kType key;
-        eType* element;
-    } cell;
-     */
     class Cell{
     public:
         Cell* next;
@@ -19,7 +16,6 @@ class Map {
             element = newElementPtr;
         }
         ~Cell(){
-            delete key;
             delete element;
         }
     };
@@ -27,19 +23,73 @@ class Map {
 private:
     Cell* last;
     Cell* first;
-    Cell* current;
+    bool checkKeyExist(const kType& key) const{
+        Cell* current = first;
+        while (current){
+            if(current->key == key){
+                return true;
+            }
+            current = current->next;
+        }
+        return false;
+    }
 public:
     Map(){
         last = nullptr;
         first = nullptr;
-        current = nullptr;
+        //current = nullptr;
     }
-    void add(const kType& key, const eType& element){
 
+    ~Map(){
+        Cell* current = first;
+        Cell* temp;
+        while (current){
+            temp = current->next;
+            delete current;
+            current = temp;
+        }
+    }
+
+    void add(const kType& key, const eType element){
+        if(!checkKeyExist(key)){
+            cout << "KEY exist\n";
+        }
+        if(last){
+            last->next = new Cell(key, new eType(element));
+            last = last->next;
+        } else{
+            last = new Cell(key, new eType(element));
+            first = last;
+        }
     }
 
     eType* find(const kType& key) const{
+        Cell* current = first;
+        while (current){
+            if(current->key == key){
+                return current->element;
+            }
+            current = current->next;
+        }
+        return nullptr;
+    }
 
+    friend ostream&operator << (ostream& out, const Map& src){
+        Cell* current= src.first;
+        while (current){
+            out << current->key << "\n" << *current->element << endl;
+            current = current->next;
+        }
+        return out;
+    }
+
+    Map& operator = (const Map& src){
+        Cell* current = src.first;
+        while (current){
+            add(current->key, *current->element);
+            current = current->next;
+        }
+        return *this;
     }
 }
 ;
